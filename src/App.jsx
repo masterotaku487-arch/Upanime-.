729ee678daab4f6b3976e2a4064670627f39209c
masterotaku487-arch <masterotaku487@gmail.com>
2026-03-04 22:26:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { FavoritesProvider } from './context/FavoritesContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -9,27 +10,31 @@ import SearchPage from './pages/SearchPage'
 import CategoryPage from './pages/CategoryPage'
 import GenresPage from './pages/GenresPage'
 import LegalPage from './pages/LegalPage'
+import FavoritesPage from './pages/FavoritesPage'
 
 export default function App() {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/anime/:id" element={<AnimePage />} />
-            <Route path="/watch/:id" element={<WatchPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/category/:type" element={<CategoryPage />} />
-            <Route path="/genres" element={<GenresPage />} />
-            <Route path="/genres/:genreId" element={<CategoryPage />} />
-            <Route path="/termos" element={<LegalPage />} />
-            <Route path="/privacidade" element={<LegalPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <FavoritesProvider>
+        <div className="app">
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/anime/:id" element={<AnimePage />} />
+              <Route path="/watch/:id" element={<WatchPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/category/:type" element={<CategoryPage />} />
+              <Route path="/genres" element={<GenresPage />} />
+              <Route path="/genres/:genreId" element={<CategoryPage />} />
+              <Route path="/termos" element={<LegalPage />} />
+              <Route path="/privacidade" element={<LegalPage />} />
+              <Route path="/favoritos" element={<FavoritesPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </FavoritesProvider>
     </AuthProvider>
   )
 }
