8ab86f4387cd78a87bf80dfe1a119f65c615340e
masterotaku487-arch <masterotaku487@gmail.com>
2026-03-06 21:33:35 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { AuthProvider } from './context/AuthContext'
-import { FavoritesProvider } from './context/FavoritesContext'
+import { FavoritesProvider, useFavorites } from './context/FavoritesContext'
 import Navbar from './components/Navbar'
 import BottomNav from './components/BottomNav'
 import Footer from './components/Footer'
@@ -15,32 +16,55 @@ import LegalPage from './pages/LegalPage'
 import FavoritesPage from './pages/FavoritesPage'
 import NoticiasPage from './pages/NoticiasPage'
 import ConfigPage from './pages/ConfigPage'
+import ApiStatusPage from './pages/ApiStatusPage'
+import { checkNewEpisodes, notifEnabled } from './services/notifications'
+
+// Checa novos eps dos favoritos (roda uma vez ao abrir o site)
+function NotifChecker() {
+  const { favorites } = useFavorites()
+  useEffect(() => {
+    if (!notifEnabled() || !favorites?.length) return
+    const timer = setTimeout(() => checkNewEpisodes(favorites), 5000)
+    return () => clearTimeout(timer)
+  }, [favorites])
+  return null
+}
+
+function AppInner() {
+  const { pathname } = useLocation()
+  const isWatch = pathname.startsWith('/watch/')
+  return (
+    <div className="app">
+      {!isWatch && <Navbar />}
+      <main>
+        <Routes>
+          <Route path="/"                  element={<Home />} />
+          <Route path="/anime/:id"         element={<AnimePage />} />
+          <Route path="/watch/:id"         element={<WatchPage />} />
+          <Route path="/search"            element={<SearchPage />} />
+          <Route path="/category/:type"    element={<CategoryPage />} />
+          <Route path="/genres"            element={<GenresPage />} />
+          <Route path="/genres/:genreId"   element={<CategoryPage />} />
+          <Route path="/explorar"          element={<ExplorarPage />} />
+          <Route path="/termos"            element={<LegalPage />} />
+          <Route path="/privacidade"       element={<LegalPage />} />
+          <Route path="/favoritos"         element={<FavoritesPage />} />
+          <Route path="/novidades"         element={<NoticiasPage />} />
+          <Route path="/config"            element={<ConfigPage />} />
+          <Route path="/api-status"        element={<ApiStatusPage />} />
+        </Routes>
+      </main>
+      {!isWatch && <BottomNav />}
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <AuthProvider>
       <FavoritesProvider>
-        <div className="app">
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/anime/:id" element={<AnimePage />} />
-              <Route path="/watch/:id" element={<WatchPage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/category/:type" element={<CategoryPage />} />
-              <Route path="/genres" element={<GenresPage />} />
-              <Route path="/genres/:genreId" element={<CategoryPage />} />
-              <Route path="/explorar" element={<ExplorarPage />} />
-              <Route path="/termos" element={<LegalPage />} />
-              <Route path="/privacidade" element={<LegalPage />} />
-              <Route path="/favoritos" element={<FavoritesPage />} />
-              <Route path="/novidades" element={<NoticiasPage />} />
-              <Route path="/config" element={<ConfigPage />} />
-            </Routes>
-          </main>
-          <BottomNav />
-        </div>
+        <NotifChecker />
+        <AppInner />
       </FavoritesProvider>
     </AuthProvider>
   )
