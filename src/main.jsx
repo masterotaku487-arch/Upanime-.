2ba23a9a3273e67febed11343e09d4dd03270f08
masterotaku487-arch <masterotaku487@gmail.com>
2026-05-13 21:59:46 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// Registra Service Worker para injetar Referer nos requests do CDN AnimeFire
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .then(reg => console.log('[SW] registrado:', reg.scope))
+      .catch(err => console.warn('[SW] falhou:', err))
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
