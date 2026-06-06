b0c7b1bdeb6d5388dd9fe7f72ad88163e0e9cc27
masterotaku487-arch <masterotaku487@gmail.com>
2026-06-06 14:24:19 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',   // ← obrigatório para IPFS (caminhos relativos)
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
