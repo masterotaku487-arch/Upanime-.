1cdb7a0117b372a30dc04174cc4011c2291cbd4a
masterotaku487-arch <masterotaku487@gmail.com>
2026-06-06 19:34:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // ← obrigatório para IPFS (caminhos relativos)
+  base: '/',
   server: {
     port: 3000,
   },
