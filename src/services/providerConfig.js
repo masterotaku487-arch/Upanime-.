8e4ace4458d91a384c535bc729f04dedb33b9ce8
masterotaku487-arch <masterotaku487@gmail.com>
2026-09-05 23:02:59 -0300

    Change USE_ANIMEFIRE to true for testing mode

--- src/services/providerConfig.js
+++ src/services/providerConfig.js
@@ -1,7 +1,7 @@
 // Provedor ativo do player.
 // false = Shinokai (padrão)
 // true  = AnimeFire (modo de teste)
-export const USE_ANIMEFIRE = false
+export const USE_ANIMEFIRE = true
 
 // true: se o provedor principal falhar, tenta o outro automaticamente.
 export const AUTO_FALLBACK_PROVIDER = true
